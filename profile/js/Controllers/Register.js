08d36283f268a6cf1f1f837c30145d6c80d05aa3
smariusdp <2013056549@ust-ics.mygbiz.com>
2016-07-12 15:04:07 +0800

    [Job Seeker] input validation (email and password matching) still lacks the ff: > "Email already taken." > front-end design

--- profile/js/Controllers/Register.js
+++ profile/js/Controllers/Register.js
@@ -4,40 +4,19 @@ app.controller('Register', function(
 {
 
     $scope.form = {};
-
+    
     $scope.register = function(){
-        var errCtr = 0;
-        
-        if ($scope.form.password1 !== $scope.form.password2) {
-            errCtr++;
-            console.log("Password Mismatch");
-        }
 
-        if (!validateEmail($scope.form.email)) {
-            errCtr++;
-            console.log("Invalid Email");
-        }
+        var promise = RegisterFactory.save($scope.form);
+        promise.then(function(data){
+            console.log('Registration Successful');
+            
+        })
+        .then(null, function(data){
+            //failed to save
+            console.log('Register Failed');
+        });
 
-        
-        if (errCtr > 0) {
-            console.log('Register Failed..' + errCtr);
-        }
-        else {
-            var promise = RegisterFactory.save($scope.form);
-            promise.then(function(data){
-                console.log('Registration Successful');
-                
-            })
-            .then(null, function(data){
-                //failed to save
-                console.log('Register Failed');
-            });
-        }
     };
-
-    function validateEmail(email) {
-        var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return regex.test(email);
-    }
 
 });
--- profile/js/module.js
+++ profile/js/module.js
@@ -77,4 +77,20 @@ app.config(function($routeProvider){
             $cookies.put(pin, 'chrs201400104', { expires : exp });
         }
     };
-});
+})
+
+.directive('pwCheck', [function () {
+    return {
+      require: 'ngModel',
+      link: function (scope, elem, attrs, ctrl) {
+        var firstPassword = '#' + attrs.pwCheck;
+        elem.add(firstPassword).on('keyup', function () {
+          scope.$apply(function () {
+            var v = elem.val()===$(firstPassword).val();
+            ctrl.$setValidity('pwmatch', v);
+          });
+        });
+      }
+    }
+  }])
+;
