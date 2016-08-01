d05c52262180c7f0f4aadf5bdaceaad63b330a0f
smariusdp <2013056549@ust-ics.mygbiz.com>
2016-08-01 08:29:02 +0800

    >relocated third party scripts >added third party ng-file-upload

--- employer/js/Controllers/Posts.js
+++ employer/js/Controllers/Posts.js
@@ -1,11 +1,13 @@
-napp.controller('Posts', function(
+app.controller('Posts', function(
 									$scope,
                                     md5,
                                     $cookies,
                                     CandidatesFactory,
                                     EmployersFactory,
                                     PINService,
-                                    JobPostsFactory
+                                    JobPostsFactory,
+                                    Upload,
+                                    $timeout
 								){
 
     // $scope.candidates = {
@@ -211,5 +213,28 @@ napp.controller('Posts', function(
             //failed to save
         });
     };
+
+    $scope.uploadFiles = function(file, errFiles) {
+        $scope.f = file;
+        $scope.errFile = errFiles && errFiles[0];
+        if (file) {
+            file.upload = Upload.upload({
+                url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
+                data: {file: file}
+            });
+
+            file.upload.then(function (response) {
+                $timeout(function () {
+                    file.result = response.data;
+                });
+            }, function (response) {
+                if (response.status > 0)
+                    $scope.errorMsg = response.status + ': ' + response.data;
+            }, function (evt) {
+                file.progress = Math.min(100, parseInt(100.0 * 
+                                         evt.loaded / evt.total));
+            });
+        }   
+    }
 
 });
