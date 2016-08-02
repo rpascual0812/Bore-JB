57e152c0bea6b520c0cb31af8b120d20682267a1
Sabrina Tan <jstan.chrs@gmail.com>
2016-08-02 18:04:13 +0800

    Profile forms with css

--- jobber/js/Controllers/Feeds.js
+++ jobber/js/Controllers/Feeds.js
@@ -37,9 +37,15 @@ app.controller('Feeds', function(
     $scope.view.education='ng-hide';
     $scope.view.achievements='ng-hide';
     $scope.view.work='ng-hide';
-    
-
-    
+    $scope.months=["January","February","March","April","May","June","July","August","September","October","November","December"];
+    $scope.gender=["Male","Female","Other"];
+    $scope.countries=["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua & Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","St. Lucia","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
+    $scope.educational_attainment=["Highschool", "Vocational Course","Associate's Degree","Bachelor's Degree","Master's Degree","Doctorate"];
+    $scope.days=[];
+    $scope.work_end='ng-show';
+    $scope.graduation_years=[];
+    $scope.completed= 'ng-hide';
+    $scope.present='ng-hide';
     $scope.feeds.data = 
     [
         {
@@ -191,6 +197,7 @@ app.controller('Feeds', function(
             // });
             get_profile();
 
+
         }
     }
 
@@ -207,12 +214,13 @@ app.controller('Feeds', function(
         promise.then(function(data){
             $scope.profile = data.data.result[0];
             $scope.profile.profile = JSON.parse($scope.profile.profile);
-         //   console.log($scope.profile.profile);
+          // $scope.name = $scope.profile.profile.personal.first_name + " " + $scope.profile.profile.personal.last_name;
+          // console.log($scope.profile.profile);
             //if($scope.profile.suspended == false){
                 reload_ads();
                 reload_feeds();
                 check();
-
+                count();
             //}            
             
         });
@@ -314,8 +322,6 @@ app.controller('Feeds', function(
         $scope.update = function(word){
 
 
-            console.log($scope.today);
-
             var form = {};
             if(word === 'personal_info'){
                 form.info = JSON.stringify($scope.profile_form.personal);
@@ -337,6 +343,7 @@ app.controller('Feeds', function(
                 form.type = word;
                 $scope.view.education = 'ng-hide';
                 $scope.view.work = 'ng-show';
+            
             }else if(word === 'work'){
                 form.info = JSON.stringify($scope.profile_form.work);
                 form.type = word;
@@ -378,16 +385,17 @@ app.controller('Feeds', function(
         function check(){
             if(!$scope.profile.profile.hasOwnProperty('personal_info')){
                 $scope.view.personal_info='ng-show';
-                console.log("personal_info");
+                 // document.getElementById("name").value = $scope.name;
+                //console.log("personal_info");
             }else if(!$scope.profile.profile.hasOwnProperty('achievements')){
                 $scope.view.achievements='ng-show';
-                console.log("achievements");
+                //console.log("achievements");
             }else if(!$scope.profile.profile.hasOwnProperty('education')){
                 $scope.view.education='ng-show';
-                console.log("education");
+                //console.log("education");
             }else if(!$scope.profile.profile.hasOwnProperty('work')){
                 $scope.view.work='ng-show';
-                console.log("WORK");
+              //  console.log("WORK");
             }else{
                 $scope.view.personal_info='ng-hide';
                 $scope.view.education='ng-hide';
@@ -395,6 +403,43 @@ app.controller('Feeds', function(
                 $scope.view.work='ng-hide';
 
             }
+        }
+
+
+        $scope.count = function(){
+            count();
+        }
+        function count(){
+            for(var i=0;i<31;i++){
+                $scope.days[i]=i+1;
+            }
+            for(var i=0;i<11;i++){
+                $scope.graduation_years[i]=2016+i;
+            }
+
+        }
+
+        $scope.status = function(value){
+            if(value=='ongoing'){
+                $scope.present='ng-show';
+                $scope.completed='ng-hide';
+            }else if(value=='completed'){
+                $scope.completed='ng-show';
+                $scope.present='ng-hide';
+            }else{
+                $scope.completed='ng-hide';
+                $scope.present='ng-hide';
+            }
+
+        }
+        $scope.isEmployed = function(value){
+            console.log(value);
+            if(value){
+                $scope.work_end='ng-hide';
+                }else{
+                $scope.work_end='ng-show';
+            }
+
         }
 
 });
